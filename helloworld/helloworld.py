916c93bcb2cfd59a36a427c87028e1fb244fe03b
nicol337 <nkl9092@gmail.com>
2014-08-04 10:21:05 +0400

    added files

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, Nicole!')
+		self.response.write('Hello, Nicole and the Rest of the World!!!')
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
