0980c188a9a767265dfc834c01fd7eea643887a4
nicol337 <nkl9092@gmail.com>
2014-08-17 01:03:38 +0400

    working on registration process

--- saad.py
+++ saad.py
@@ -88,13 +88,21 @@ class HomePage(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 class Registration(webapp2.RequestHandler):
-    error_msg = ""
 
     def post(self):
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
         user = users.get_current_user()
         error_msg = ""
         new_team_name = self.request.get('team_name')
         new_team_email = self.request.get('team_email')
+
 
         existing_team_emails_query = db.GqlQuery("SELECT * FROM Team " +
                                             "WHERE team_email = :1", new_team_email)
@@ -123,23 +131,32 @@ class Registration(webapp2.RequestHandler):
             template_values = { 
                 'user' : user,
                 'url': url,
-                'url_linktext': url_linktext
+                'url_linktext': url_linktext,
+                'team_name': new_team_name,
+                'team_email': new_team_email
             } 
 
-            template = JINJA_ENVIRONMENT.get_template('team_home_page.html')
+            # template = JINJA_ENVIRONMENT.get_template('team_home_page.html')
+            # self.response.write(template.render(template_values))
+            # self.redirect('/team/' + new_team_name)
+        
+    
+            template = JINJA_ENVIRONMENT.get_template('home_page.html')
             self.response.write(template.render(template_values))
-            self.redirect('/team/' + new_team.get_team_name())s
 
         else:
             template_values = { 
                 'user' : user,
                 'url': url,
                 'url_linktext': url_linktext,
-                'error_msg': error_msg
+                'error_msg': error_msg,
+                'team_name': new_team_name,
+                'team_email': new_team_email
             }   
         
             template = JINJA_ENVIRONMENT.get_template('registration.html')
             self.response.write(template.render(template_values))
+
 
 
     def get(self):
@@ -155,8 +172,7 @@ class Registration(webapp2.RequestHandler):
         template_values = { 
             'user' : user,
             'url': url,
-            'url_linktext': url_linktext,
-            'error_msg': error_msg
+            'url_linktext': url_linktext
         }   
     
         template = JINJA_ENVIRONMENT.get_template('registration.html')
@@ -226,103 +242,18 @@ class UserHome(webapp2.RequestHandler):
 class TeamHome(webapp2.RequestHandler):
 
     def post(self, blog_name, page_number):
-        if self.request.get('blogpost_title') and self.request.get('blogpost_content'):
-            blogpost_title = self.request.get('blogpost_title')
-            blogpost_content = self.request.get('blogpost_content')
-            
-            blogpost_tags = self.request.get('blogpost_tags')
-            tag_tokens = blogpost_tags.split(',')  
-
-            blogpost = Blogpost(author=users.get_current_user(), title=blogpost_title, content=blogpost_content, blog=blog_name, tags=tag_tokens)
-            blogpost.put()
-
-            self.redirect('/blog/' + blog_name + '/')
-        else:
-            self.redirect('/blog/' + blog_name + '/')  
+        user = users.get_current_user()
 
 
     def get(self, team_name):
 
         user = users.get_current_user()
 
-
-        owner = False
-        if users.get_current_user():
-            log_in_out_url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            template_url = 'home_page.html'
-            log_in_out_url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        one_blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE title = :1", blog_name)
-
-        one_blog = one_blog_query.run(limit=1)
-        
-        for blog in one_blog:
-            if user == blog.author:
-                owner = True
-            else:
-                owner = False
-
-        blogpost_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 " +
-                "ORDER BY date DESC", blog_name)
-
-        blogposts = blogpost_query.run(offset=(ViewingPage+1)*10)
-
-        number_of_posts_left = 0
-
-        for blog in blogposts:
-            number_of_posts_left+=1
-
-        if number_of_posts_left:
-            moreposts = True
-        else:
-            moreposts = False
-
-        blogposts = blogpost_query.run(offset=ViewingPage*10,limit=10)
-
-        blogpost_content = {}
-        for post in blogpost_query.run(offset=ViewingPage*10,limit=10):
-            blogpost_content[post.title]=post.content
-            if len(post.content) > 500:
-                blogpost_content[post.title]=post.content[:500]
-
-        for key in blogpost_content.keys():
-            text_tokens = blogpost_content[key].split(' ')
-            for tok in text_tokens:
-                if tok.startswith("http://") or tok.startswith("https://"):
-                    blogpost_content[key]=blogpost_content[key].replace(tok,to_link(tok))
-
-        blog_tags=[]
-        for post in blogpost_query.run():
-            for tag in post.tags:
-                if tag not in blog_tags:
-                    blog_tags.append(tag)
-            
-
-        blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE author = :1 " +
-                "ORDER BY title", user)
-        blogs = blog_query.run(limit=1000)    
-
         template_values = { 
             'user' : user,
-            'url': log_in_out_url,
-            'url_linktext': url_linktext,
-            'blogs' : blogs,
-            'blogposts' : blogposts,
-            'blog_name': blog_name,
-            'one_blog': one_blog,
-            'blogpost_content' : blogpost_content,
-            'blog_tags': blog_tags,
-            'owner' : owner,
-            'moreposts' : moreposts,
-            'page_counter': ViewingPage
-        } 
-        template = JINJA_ENVIRONMENT.get_template("blog_home_page.html")
+        }
+       
+        template = JINJA_ENVIRONMENT.get_template("team_home_page.html")
         self.response.write(template.render(template_values))
 
 class BlogHome(webapp2.RequestHandler):
